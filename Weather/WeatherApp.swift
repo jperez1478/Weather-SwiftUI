68b9b94bbbb0f785219d80aef80159fbfd14c938
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-07-23 17:14:57 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jessica Perez on 7/23/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
